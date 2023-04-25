d21e4905add13dfdd8cdba296d337cec3273d5db
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-26 01:40:24 +0200

    added functionality to check if timer is valid or not on modeljs

--- model.js
+++ model.js
@@ -1,4 +1,20 @@
-export function startCounter(TimerData){
-  const [hour,minute,second]=TimerData
-  console.log(hour, minute,second);
+export let state = [];
+
+export function startCounter(TimerData) {
+  // debugger
+  let [hour, minute, second] = TimerData;
+  if (hour === 0 && minute === 0 && second === 0) {
+    state = [-1];
+    return;
+  }
+
+  if (hour > 24) hour = 24;
+  if (hour < 0) hour = 0;
+  if (minute > 60) minute = 59;
+  if (minute < 0) minute = 0;
+  if (second > 60) second = 59;
+  if (second < 0) second = 0;
+  state = [hour, minute, second];
+
+  console.log(state);
 }
