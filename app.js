cb06f1a51cab0c89269024ec73fe7a4afc82060a
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-26 01:34:51 +0200

    added functionality to render error message and current time on app.s

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 // Controller
 import timer from "./timerView.js";
-import * as model from './model.js'
+import * as model from "./model.js";
 function controlStart() {
-  // 1. Set buttons and inputs in start status
-  debugger
+  // 1. Start the counter
+  model.startCounter(timer.getTimerData());
+
+  // 2 Check if the timer is valid, -1 means invalid timer
+  if (model.state.length === 1) {
+    timer.renderError();
+    timer.initialStatus();
+    return;
+  }
+
+  // 3. Set buttons and inputs in start status
   timer.startStatus();
-  // 2. Start the counter
-  model.startCounter(timer.getTimerData()) 
+
+  // 4 Render the current time
+  timer.render();
 }
 
-function controlPause(){
+function controlPause() {
   // 1. Set buttons and inputs in pause status
   timer.pauseStatus();
   // 2. Start the counter
-  model.startCounter(timer.getTimerData())
+  model.startCounter(timer.getTimerData());
 }
 
-function controlStop(){
+function controlStop() {
   // 1. Set buttons and inputs in stop status
   timer.initialStatus();
   // 2. Start the counter
-  model.startCounter(timer.getTimerData())
+  model.startCounter(timer.getTimerData());
 }
 
 init();
@@ -29,5 +39,5 @@ function init() {
   timer.addHandlerStart(controlStart);
   timer.addHandlerPause(controlPause);
   timer.addHandlerStop(controlStop);
-  timer.initialStatus()
+  timer.initialStatus();
 }
