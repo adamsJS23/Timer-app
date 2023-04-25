e2aebe6f7af0b8dc8a63cff86c33e65fb093b20b
adamsJS23 <adamaguebre@yahoo.fr>
2023-04-26 00:27:56 +0200

    implement disable/enabale btn and ipt on app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // Controller
+import timer from "./timerView.js";
+import * as model from './model.js'
+function controlStart() {
+  // 1. Set buttons and inputs in start status
+  debugger
+  timer.startStatus();
+  // 2. Start the counter
+  model.startCounter(timer.getTimerData()) 
+}
+
+function controlPause(){
+  // 1. Set buttons and inputs in pause status
+  timer.pauseStatus();
+  // 2. Start the counter
+  model.startCounter(timer.getTimerData())
+}
+
+function controlStop(){
+  // 1. Set buttons and inputs in stop status
+  timer.initialStatus();
+  // 2. Start the counter
+  model.startCounter(timer.getTimerData())
+}
+
+init();
+
+function init() {
+  timer.addHandlerStart(controlStart);
+  timer.addHandlerPause(controlPause);
+  timer.addHandlerStop(controlStop);
+  timer.initialStatus()
+}
